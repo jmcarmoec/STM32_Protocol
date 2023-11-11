346cf7f67547d86e2fb3b39ef4a636e9ada7e708
JoaoMateus <joaomateus102030@gmail.com>
2023-11-10 23:49:57 -0300

    first commit in firmware

---
+++ Firmware/STM32CubeMX/Firmware_STM32_Protocol/STM32CubeIDE/Debug/Application/User/Startup/startup_stm32l496zgtxp.d
@@ -0,0 +1,2 @@
+Application/User/Startup/startup_stm32l496zgtxp.o: \
+ ../Application/User/Startup/startup_stm32l496zgtxp.s
---
+++ Firmware/STM32CubeMX/Firmware_STM32_Protocol/STM32CubeIDE/Debug/Application/User/ThreadSafe/newlib_lock_glue.d
@@ -0,0 +1,26 @@
+Application/User/ThreadSafe/newlib_lock_glue.o: \
+ ../Application/User/ThreadSafe/newlib_lock_glue.c \
+ C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Application/User/ThreadSafe/stm32_lock.h \
+ C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ ../../Core/Inc/FreeRTOSConfig.h \
+ C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Drivers/CMSIS/Include/cmsis_gcc.h:
+../Application/User/ThreadSafe/stm32_lock.h:
+C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+../../Core/Inc/FreeRTOSConfig.h:
+C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+C:/Users/joaom/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.0/Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
